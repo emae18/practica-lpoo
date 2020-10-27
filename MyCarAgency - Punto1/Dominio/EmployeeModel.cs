146dda65a5348f7df9dafe4f2c399635e9627e6e
HChaza <hchazarreta12@gmail.com>
2020-10-27 19:07:00 -0300

    Agregue Clase EmployeeModel

--- MyCarAgency - Punto1/Dominio/EmployeeModel.cs
+++ MyCarAgency - Punto1/Dominio/EmployeeModel.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Datos;
+using Entidades;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,26 @@ using System.Threading.Tasks;
 
 namespace Dominio
 {
-    class EmployeeModel
+    public class EmployeeModel
     {
+         public void AltaEmpleado()
+        {
+        
+        }
+
+        public void ModificarEmpleado()
+        {
+            
+        }
+
+        public void EliminarEmpleado(int id)
+        {
+            
+        }
+
+        public List<Employee> ListarEmpleados()
+        {
+            return;
+        }
     }
 }
