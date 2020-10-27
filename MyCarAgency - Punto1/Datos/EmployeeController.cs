fd2e08c45b49e9c2767a8960208817fc659ca522
paolaCussi <soledadcussi@gmail.com>
2020-10-27 18:51:26 -0300

    Creacion de Clase EmployeeControler en Datos

---
+++ MyCarAgency - Punto1/Datos/EmployeeController.cs
@@ -0,0 +1,32 @@
+﻿using Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datos
+{
+    public class EmployeeController
+    {
+        public void InsertEmployee(Employee oEmployee)
+        {
+        }
+
+        public void UpdateUser(Employee oEmployee)
+        {            
+        }
+
+        public void RemoveUser(int id)
+        {           
+        }
+
+        public List<Employee> GetUsers()
+        {
+            using (db_agencyEntities db = new db_agencyEntities())
+            {
+                return db.Employees.ToList();
+            }
+        }
+    }
+}
